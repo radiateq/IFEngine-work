36cd2264c58bf945dc88360605c9f0e1ca8623be
radiateq <radiateq@gmail.com>
2018-11-09 09:15:33 +0100

    morning commit work in progress

--- OpenGLESApp2/OpenGLESApp2.Android.NativeActivity/RQ_NDK_Utils.h
+++ OpenGLESApp2/OpenGLESApp2.Android.NativeActivity/RQ_NDK_Utils.h
@@ -31,6 +31,65 @@ template<typename Tptr_type> class RQTCAutoDelete {
  }
 };
 
+template<typename TDataType_np, bool delete_pointers_on_destructor = false, bool pointer_array = false > class S_listWrap_ptr : public std::list<TDataType_np*>
+{
+public:
+ void removeElement(TDataType_np *input_var) {
+  for (typename std::list<TDataType_np*>::iterator iter = this->begin(); iter != this->end(); iter++) {
+   if ((*iter) == input_var) {
+    if (pointer_array)
+     delete[] input_var;
+    else
+     delete input_var;
+    this->erase(iter);//this->erase(iter++) to continue execution if search again for multiple elements
+    break;
+   }
+  }
+ }
+ ~S_listWrap_ptr()
+ {
+  if (delete_pointers_on_destructor)
+  {
+   TDataType_np *temp_var;
+   while (this->size() > 0)
+   {
+    temp_var = this->back();
+    if (pointer_array)
+     delete[] temp_var;
+    else
+     delete temp_var;
+    this->pop_back();
+   }
+  }
+
+ }
+};
+
+
+//int main()
+//{
+// _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+// S_listWrap_ptr<int, true> pointerlist;
+// int *findme = new int(1);
+// pointerlist.push_back(findme);
+// pointerlist.push_back(new int(2));
+// pointerlist.push_back(new int(3));
+// pointerlist.push_back(new int(4));
+// pointerlist.removeElement(findme);
+// pointerlist.removeElement(*(pointerlist.begin()));
+// S_listWrap_ptr<int, true, true> pointerArraylist;
+// int *findmea = new int[100];
+// pointerArraylist.push_back(new int[10]);
+// pointerArraylist.push_back(new int[50]);
+// pointerArraylist.push_back(findmea);
+// pointerArraylist.push_back(new int[150]);
+// pointerArraylist.removeElement(findmea);
+// pointerArraylist.removeElement(*(pointerArraylist.begin()));
+// return 0;
+//}
+
+
+
 #define IF_NULL_DELETE(PARAM) if( NULL != PARAM ) { delete PARAM; PARAM = NULL; }
 #define IF_NULL_DELETE_ARRAY(PARAM) if( NULL != PARAM ) { delete [] PARAM; PARAM = NULL; }
 
--- OpenGLESApp2/OpenGLESApp2.Shared/IFBox2DAdapter.h
+++ OpenGLESApp2/OpenGLESApp2.Shared/IFBox2DAdapter.h
@@ -1,20 +1,28 @@
 #pragma once
 
 #include <Box2D/Box2D.h>
+#include <list>
 
 
-class ifCBox2DAdapter {
+class Iifclass1 {
 public:
- 
-};
+ ~Iifclass1() {
+  Free();
+ }
+ void Init() {
+  Free();
+ }
+private:
+ virtual void Free() = 0;
+ };
 
-class ifCBox2WorldManager {
+class ifCB2WorldManager : Iifclass1 {
 public:
- ifCBox2WorldManager() {
+ ifCB2WorldManager() {
   Gravity = NULL;
   World = NULL;
  }
- ~ifCBox2WorldManager() {
+ ~ifCB2WorldManager() {
   Free();
  }
  void MakeWorld(float32 gx, float32 gy) {
@@ -25,9 +33,6 @@ public:
 private:
  b2Vec2 *Gravity;
  b2World *World;
- void Init() {
-  Free();
- } 
  void Free() {
   IF_NULL_DELETE(Gravity)
   IF_NULL_DELETE(World)
@@ -35,34 +40,106 @@ private:
 public:
 };
 
-class ifCBox2DBodyManager {
+
+class ifCB2Body : Iifclass1 {
+public:
+ ifCB2Body() {
+  body_def = NULL;
+  body = NULL;
+ }
+ ~ifCB2Body() {
+  Free();
+ }
+ b2BodyDef *body_def;
+ b2Body *body;
+ std::list<b2FixtureDef> shape;
+ std::list<b2FixtureDef> fixture;
+ void Free() {
+  World
+  body = NULL;
+  body_def = NULL;
+  shape.clear();
+  fixture.clear();
+ }
+};
+
+class ifCB2BodyManager {
+public:
+ ifCB2BodyManager() {
+  B2WorldManager = NULL;
+
+
+
+
+  // Define the ground body.
+  b2BodyDef groundBodyDef;
+  groundBodyDef.position.Set(0.0f, -10.0f);
+
+  // Call the body factory which allocates memory for the ground body
+  // from a pool and creates the ground box shape (also from a pool).
+  // The body is also added to the world.
+  b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+  // Define the ground box shape.
+  b2PolygonShape groundBox;
+
+  // The extents are the half-widths of the box.
+  groundBox.SetAsBox(50.0f, 10.0f);
+
+  // Add the ground fixture to the ground body.
+  groundBody->CreateFixture(&groundBox, 0.0f);
+
+
+
+
+
+
+
+
+
+ }
+ ~ifCB2BodyManager() {
+  Free();
+ }
+ void MakeWorld(ifCB2WorldManager *_B2WorldManager) {
+  Init();
+  B2WorldManager = _B2WorldManager;
+ }
+private:
+ ifCB2WorldManager *B2WorldManager;
+
+
+
+ b2Vec2 *Gravity;
+ b2World *World;
+ void Init() {
+  Free();
+  
+ }
+ void Free() {  
+  IF_NULL_DELETE(Gravity)
+  IF_NULL_DELETE(World)
+ }
 public:
 
-
-
- // Define the ground body.
- b2BodyDef groundBodyDef;
- groundBodyDef.position.Set(0.0f, -10.0f);
-
- // Call the body factory which allocates memory for the ground body
- // from a pool and creates the ground box shape (also from a pool).
- // The body is also added to the world.
- b2Body* groundBody = world.CreateBody(&groundBodyDef);
-
- // Define the ground box shape.
- b2PolygonShape groundBox;
-
- // The extents are the half-widths of the box.
- groundBox.SetAsBox(50.0f, 10.0f);
-
- // Add the ground fixture to the ground body.
- groundBody->CreateFixture(&groundBox, 0.0f);
-
+};
+class ifCB2GameLoop {
 
 };
-class ifCBox2DGameLoop {
+
+
+
+class ifCB2Adapter : public ifCB2WorldManager {
+public:
+ ifCB2Adapter() {
+
+ }
+ b2Vec2 *Gravity;
+ b2World *World;
 
 };
+
+
 
 // Define the gravity vector.
 //b2Vec2 gravity;//(2.0f, -2.0f);
